39a328679d50accd25887f39374a222fc960821c
Eduardo <eduardo.rsbtn@eduge.ch>
2025-11-19 17:01:24 +0100

    Vue et templates pour chaque task et CSS du site

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task
 
 
 def post_list(request):
     tasks = Task.objects.all()
     return render(request, 'blog/post_list.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'blog/task_detail.html', {'task': task})
