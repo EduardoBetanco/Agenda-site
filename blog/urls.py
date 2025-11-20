bbc151a3b30c85d1a603bdbd4b4319ddd068dfc5
Eduardo <eduardo.rsbtn@eduge.ch>
2025-11-20 16:46:56 +0100

    Ajout de création et modification de task par les utilisateurs avec droits administration

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/new/', views.task_new, name='task_new'),
+    path('task/<int:pk>/edit/', views.task_edit, name='task_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Task
+from .forms import TaskForm
+
 
 
 def post_list(request):
@@ -9,3 +12,30 @@ def post_list(request):
 def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'blog/task_detail.html', {'task': task})
+
+def task_new(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.created_date = timezone.now()
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm()
+    return render(request, 'blog/task_edit.html', {'form': form})
+
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.created_date = timezone.now()
+            task.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'blog/task_edit.html', {'form': form})
+
+
