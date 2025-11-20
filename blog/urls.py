d979afb2046830c48b12645e4923bccf0c502e74
Eduardo <eduardo.rsbtn@eduge.ch>
2025-11-20 17:42:08 +0100

    Modification du visuelle et ajout de supresion des tasks et précision si faite ou non

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.task_list, name='task_list'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
     path('task/new/', views.task_new, name='task_new'),
     path('task/<int:pk>/edit/', views.task_edit, name='task_edit'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+    path('task/<int:pk>/toggle_done/', views.task_toggle_done, name='task_toggle_done'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 
 
-def post_list(request):
+def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'blog/post_list.html', {'tasks': tasks})
+    return render(request, 'blog/task_list.html', {'tasks': tasks})
+
 
 def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'blog/task_detail.html', {'task': task})
+
 
 def task_new(request):
     if request.method == "POST":
@@ -24,6 +27,7 @@ def task_new(request):
     else:
         form = TaskForm()
     return render(request, 'blog/task_edit.html', {'form': form})
+
 
 def task_edit(request, pk):
     task = get_object_or_404(Task, pk=pk)
@@ -39,3 +43,17 @@ def task_edit(request, pk):
     return render(request, 'blog/task_edit.html', {'form': form})
 
 
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.delete()
+        messages.success(request, "La tâche a été supprimée.")
+        return redirect('task_list')
+    return render(request, 'blog/task_delete.html', {'task': task})
+
+
+def task_toggle_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.status = not task.status
+    task.save()
+    return redirect('task_detail', pk=pk)
